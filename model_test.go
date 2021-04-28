4fcbe83406da09412abaea924b69ba6a54b08eb1
wuhuizuo <wuhuizuo@126.com>
2021-04-28 19:52:42 +0800

    test(table): rename files/functions/structs

--- model_test.go
+++ model_test.go
@@ -4,7 +4,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-// RecordTest record for unit testing
+// testRecord record for unit testing
 // +--------------+---------------+------+-----+-------------------+----------------+------------+
 // | Field        | Type          | Null | Key | Default           | Extra          | 备注       |
 // +--------------+---------------+------+-----+-------------------+----------------+------------+
@@ -19,7 +19,7 @@ import (
 // | title        | varchar(128)  | NO   |     | NULL              |                |告警标题     |
 // | body         | varchar(1024) | NO   |     | NULL              |                |告警内容     |
 // +--------------+---------------+------+-----+-------------------+----------------+------------+
-type RecordTest struct {
+type testRecord struct {
 	ID           int32      `json:"id,omitempty"           db:"id,type=INT,auto_increment,key"`
 	ProjectID    int32      `json:"projectId"              db:"projectId,type=INT,not_null,split"`
 	RuleID       int32      `json:"ruleId,omitempty"       db:"ruleId,type=INT,not_null,primary"`
@@ -32,65 +32,65 @@ type RecordTest struct {
 	Body         string     `json:"body,omitempty"         db:"body,type=VARCHAR(1024),not_null"`
 }
 
-// TableTest for unit testing
-type TableTest Table
+// testTable for unit testing
+type testTable Table
 
 // RowModel model for store
-func (t *TableTest) RowModel() interface{} {
-	return &RecordTest{}
+func (t *testTable) RowModel() interface{} {
+	return &testRecord{}
 }
 
 // Schema of table
-func (t *TableTest) Schema() *TableSchema {
+func (t *testTable) Schema() *TableSchema {
 	return Schema(t, (*Table)(t))
 }
 
 // Create of table
-func (t *TableTest) Create() error {
+func (t *testTable) Create() error {
 	return Create(t)
 }
 
 // IsDup record in Table?
-func (t *TableTest) IsDup(record interface{}) (interface{}, error) {
+func (t *testTable) IsDup(record interface{}) (interface{}, error) {
 	return IsDup(t, record)
 }
 
 // Insert record to Table
-func (t *TableTest) Insert(record interface{}) (int64, error) {
+func (t *testTable) Insert(record interface{}) (int64, error) {
 	return Insert(t, record)
 }
 
 // Inserts records to Table
-func (t *TableTest) Inserts(records []interface{}) ([]int64, error) {
+func (t *testTable) Inserts(records []interface{}) ([]int64, error) {
 	return Inserts(t, records)
 }
 
 // Delete records from Table
-func (t *TableTest) Delete(filter RowFilter) error {
+func (t *testTable) Delete(filter RowFilter) error {
 	return Delete(t, filter)
 }
 
 // Save record
-func (t *TableTest) Save(record interface{}) error {
+func (t *testTable) Save(record interface{}) error {
 	return Save(t, record)
 }
 
 // Update records in Table
-func (t *TableTest) Update(filter RowFilter, updateParts map[string]interface{}) error {
+func (t *testTable) Update(filter RowFilter, updateParts map[string]interface{}) error {
 	return Update(t, filter, updateParts)
 }
 
 // List records from Table
-func (t *TableTest) List(filter RowFilter, options ListOptions) ([]interface{}, error) {
+func (t *testTable) List(filter RowFilter, options ListOptions) ([]interface{}, error) {
 	return List(t, filter, options)
 }
 
 // Get record by Key
-func (t *TableTest) Get(filter RowFilter, record interface{}) error {
+func (t *testTable) Get(filter RowFilter, record interface{}) error {
 	return GetFirst(t, filter, record)
 }
 
 // ScanRow scan struct from table row
-func (t *TableTest) ScanRow(rows *sqlx.Rows) (interface{}, error) {
+func (t *testTable) ScanRow(rows *sqlx.Rows) (interface{}, error) {
 	return ScanRow(t, rows)
 }
