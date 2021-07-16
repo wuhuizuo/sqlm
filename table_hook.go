cf6407cb897105b3e1c51516374c4d4c2fa7389c
wuhuizuo <wuhuizuo@126.com>
2021-07-16 21:26:10 +0800

    refactor: decrease complex for `TableHooks#Merge()`

--- table_hook.go
+++ table_hook.go
@@ -21,6 +21,15 @@ type TableOperateHook struct {
 	After  []interface{}
 }
 
+func (h *TableOperateHook) Merge(other *TableOperateHook) {
+	if other == nil {
+		return
+	}
+
+	h.Before = append(h.Before, other.Before...)
+	h.After = append(h.After, other.After...)
+}
+
 // TableHooks for all operation before and after for the table
 type TableHooks struct {
 	Insert  TableOperateHook
@@ -31,39 +40,14 @@ type TableHooks struct {
 }
 
 // Merge with other hooks
-func (h *TableHooks) Merge(newHooks *TableHooks) {
-	if newHooks == nil {
+func (h *TableHooks) Merge(other *TableHooks) {
+	if other == nil {
 		return
 	}
 
-	if len(newHooks.Insert.Before) > 0 {
-		h.Insert.Before = append(h.Insert.Before, newHooks.Insert.Before...)
-	}
-	if len(newHooks.Insert.After) > 0 {
-		h.Insert.After = append(h.Insert.After, newHooks.Insert.After...)
-	}
-	if len(newHooks.Inserts.Before) > 0 {
-		h.Inserts.Before = append(h.Inserts.Before, newHooks.Inserts.Before...)
-	}
-	if len(newHooks.Insert.After) > 0 {
-		h.Inserts.After = append(h.Inserts.After, newHooks.Inserts.After...)
-	}
-	if len(newHooks.Update.Before) > 0 {
-		h.Update.Before = append(h.Update.Before, newHooks.Update.Before...)
-	}
-	if len(newHooks.Insert.After) > 0 {
-		h.Update.After = append(h.Update.After, newHooks.Update.After...)
-	}
-	if len(newHooks.Save.Before) > 0 {
-		h.Save.Before = append(h.Save.Before, newHooks.Save.Before...)
-	}
-	if len(newHooks.Insert.After) > 0 {
-		h.Save.After = append(h.Save.After, newHooks.Save.After...)
-	}
-	if len(newHooks.Delete.Before) > 0 {
-		h.Delete.Before = append(h.Delete.Before, newHooks.Delete.Before...)
-	}
-	if len(newHooks.Insert.After) > 0 {
-		h.Delete.After = append(h.Delete.After, newHooks.Delete.After...)
-	}
+	h.Insert.Merge(&other.Insert)
+	h.Inserts.Merge(&other.Insert)
+	h.Save.Merge(&other.Insert)
+	h.Update.Merge(&other.Insert)
+	h.Delete.Merge(&other.Insert)
 }
